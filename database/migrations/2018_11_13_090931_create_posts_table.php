8dec7a51a49821738a73f51bd07c0b23fa39083f
3A532051 <chu123520@gmail.com>
2018-11-13 17:17:19 +0800

    撰寫posts的schema，增加兩個欄位

--- database/migrations/2018_11_13_090931_create_posts_table.php
+++ database/migrations/2018_11_13_090931_create_posts_table.php
@@ -14,7 +14,8 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
             $table->timestamps();
         });
     }
