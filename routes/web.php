915e4e9838653b707358a8b28c7f11ed6218ac8a
3A532051 <chu123520@gmail.com>
2018-11-14 14:28:29 +0800

    了解 Model 和 Collection 的差異

--- routes/web.php
+++ routes/web.php
@@ -39,12 +39,19 @@ Route::get('/', function () {
 
     //\App\Post::destroy(2);
 
-    \App\Post::destroy(3,5,7);
+    //\App\Post::destroy(3,5,7);
 
-//    $post = \App\Post::find(1);
-//    foreach($post->comments as $comment) {
-//        echo $comment->title.'<br>';
-//    }
+    //$allPosts=\App\Post::all();
+    //dd($allPosts);
+    //$featuredPosts=\App\Post::where('is_feature',1) ->get();
+    //dd($featuredPosts);
+
+    $fourthPost=\App\Post::find(4);
+    dd($fourthPost);
+    $lastPost=\App\Post::orderBy('id','DESC') ->first();
+    dd($lastPost);
+
+
 //    return view('welcome');
 });
 Route::get('/home', 'HomeController@index');
